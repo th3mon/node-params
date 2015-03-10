3653512991d8aae928258d2c551c89086e2c3c34
Przemyslaw Szelenberger <p.szelenberger@gmail.com>
2015-03-10 13:18:14 +0000

    Giving arg1 works fine.

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 
 var
     express = require('express'),
-    app = express();
+    app = express(),
+    args = process.argv.slice(2),
+    argName = 'arg1';
     
-process.argv.forEach(function(val, index, array) {
-  console.log(index + ': ' + val);
+args.forEach(function(arg) {
+    if (arg.indexOf(argName + '=false') !== -1) {
+        console.log(argName + ' is off');
+    }
+    
+    else if ( (arg.indexOf(argName + '=true') !== -1) || (arg.indexOf(argName) !== -1) ) {
+        console.log(argName + ' is on');
+    }
 });
-    
