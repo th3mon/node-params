8938f5343bb9dfd5b39e297a1d613db70d315cd9
Przemyslaw Szelenberger <p.szelenberger@gmail.com>
2015-03-10 14:04:50 +0000

    Refactoring: Moved code to one function.

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 'use strict';
 
-var
-    args = process.argv.slice(2),
-    validArgumets = [
-        'arg1',
-        'arg2',
-        'argN'
-    ];
+
+function cli (validArgumets) {
+    var args = process.argv.slice(2);
+        
+    args.forEach(processArguments);
+
+    function processArguments (arg) {
+        validArgumets.forEach(function processValidArguments (validArgument) {
+            if ( contains(arg, validArgument + '=false') ) {
+                console.log(validArgument + ' is off');
+            }
+            
+            else if ( contains(arg, validArgument + '=true') || contains(arg, validArgument) ) {
+                console.log(validArgument + ' is on');
+            } 
+        });
+    }
     
-function processArguments (arg) {
-    validArgumets.forEach(function processValidArguments (validArgument) {
-        if ( contains(arg, validArgument + '=false') ) {
-            console.log(validArgument + ' is off');
+    function contains(str1, str2) {
+        if (!str1 || (typeof str1 !== 'string') || !str2 || (typeof str2 !== 'string') ) {
+            return null;
         }
         
-        else if ( contains(arg, validArgument + '=true') || contains(arg, validArgument) ) {
-            console.log(validArgument + ' is on');
-        } 
-    });
-}
-
-function contains(str1, str2) {
-    if (!str1 || (typeof str1 !== 'string') || !str2 || (typeof str2 !== 'string') ) {
-        return null;
-    }
-    
-    else {
-        return str1.indexOf(str2) !== -1;
+        else {
+            return str1.indexOf(str2) !== -1;
+        }
     }
 }
 
-args.forEach(processArguments);
+cli(['arg1', 'arg2', 'argN']);
